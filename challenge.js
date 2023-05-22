710b8aaa03f58d42308f061d93177339a6528977
MustapheMS <moztaphem9@gmail.com>
2023-05-22 19:50:26 +0300

    Calculator Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,30 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+//👍👍 💯💯 😎😎
+
+function calculator(num1, num2, operator) {
+    let result;
+  
+    if (operator === "+") {
+      result = num1 + num2;
+    } else if (operator === "-") {
+      result = num1 - num2;
+    } else if (operator === "*") {
+      result = num1 * num2;
+    } else if (operator === "/") {
+      result = num1 / num2;
+    } else {
+      console.log("Invalid operator.");
+      return;
+    }
+  
+    console.log(`Jawaabtu waa: ${result}`);
+  }
+    calculator(20, 9, "+");
+    calculator(55, 5, "-"); 
+    calculator(100, 5, "*"); 
+    calculator(90, 5, "/"); 
+    calculator(90, 5, "%");
+
+
